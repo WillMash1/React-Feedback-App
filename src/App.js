c57c63487257564d0f77c55e6b990e12821e5d0f
will <wmashhadi@Gmail.com>
2022-03-24 15:13:07 +0000

    Removing unused variables

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import { useState } from "react"
 import Header from "./components/Header"
 import FeedBackList from "./components/FeedbackList"
 import FeedBackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
-import FeedbackData from "./data/FeedbackData"
-import Card from "./components/shared/Card"
 import AboutPage from "./pages/AboutPages"
 import {FeedbackProvider} from './context/FeedbackContext'
 import AboutIconLink from './components/AboutIconLink'
